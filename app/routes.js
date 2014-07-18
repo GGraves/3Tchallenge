3ea68d1a8adc637cdb80520be035826443199db4
Gavin <Gavin@JGG.email>
2014-07-17 22:19:03 -0700

    fixed formating of routes file

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,7 @@
 module.exports = function( app)
 {
-	app.get( '*', function( req, res)
+    app.get( '*', function( req, res)
     {
-	    res.sendfile( './public/index.html');
-	});
+        res.sendfile( './public/index.html');
+    });
 };
